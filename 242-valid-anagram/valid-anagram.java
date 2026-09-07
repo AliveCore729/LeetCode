d549416a80c5094b50164ac87e3cb66f7397c448
Shreyansh jain <alivecore729@gmail.com>
2026-09-07 23:55:09 +0530

    Time: 18 ms (13.59%) | Memory: 46.3 MB (42.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer> map=new HashMap<>();
+        Map<Character,Integer> map2=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+        }
+        return map.equals(map2);
+    }
+}
